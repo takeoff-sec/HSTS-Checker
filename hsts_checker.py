59d6102b0292a1a610e3235ed4bcb5d4509c57ae
Matt Barton <matty@live.co.uk>
2019-04-02 16:08:35 +0100

    Update hsts_checker.py
    Added in preload and includesubdomains checks

--- hsts_checker.py
+++ hsts_checker.py
@@ -5,8 +5,10 @@ def main():
 
 def check_sts():
 	url = raw_input("Please enter the URL you wish to check: ")
-	#url = "https://hsts.mbproject.pw"
+	#url = "https://preload.mbproject.pw"
 	response = requests.get(url)
+
+	print("Results for:", url)
 
 	#Check for presence of strict-transport-security response header
 	if 'strict-transport-security' in response.headers:
@@ -18,10 +20,19 @@ def check_sts():
 
 	#If sts header found, check for preload flag
 	if hsts_present == True:
-		if 'preload' in response.headers:
+		if 'preload' in response.headers['strict-transport-security']:
 			print("Preload flag present")
 		else:
 			print("Preload flag not present")
+	else:
+		None
+	
+	#If sts header found, check for includeSubdomains flag
+	if hsts_present == True:
+		if 'includeSubDomains' in response.headers['strict-transport-security']:
+			print("Include SubDomains flag present")
+		else:
+			print("Include SubDomains flag not present")
 	else:
 		None
 main()
